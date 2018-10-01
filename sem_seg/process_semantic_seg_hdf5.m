0b89630cfb35f69d14a7c458766cce387fefa482
Ling Zhang <dearlingzhang@gmail.com>
2018-10-01 09:18:41 -0400

    process semantic seg hdf5 data

---
+++ sem_seg/process_semantic_seg_hdf5.m
@@ -0,0 +1,87 @@
+% read data
+mainpath = 'indoor3d_sem_seg_hdf5_data';
+
+data_path = strcat( mainpath, '/*.h5');
+data_files = dir(data_path);
+
+%     label_path = strcat( mainpath, '/', category, '/points_label', '/*.seg');
+%     label_files = dir(label_path);
+
+mkdir indoor3d_sem_seg_hdf5_data coords_normal;
+
+for n=1:length(data_files)
+    data_path = strcat( mainpath, '/', data_files(n).name);
+    
+    h5disp(data_path);
+    data = h5read(data_path,'/data');
+    label = h5read(data_path,'/label');
+    
+    x = length(data(:, 1, 1)) + 3;
+    y = length(data(1, :, 1));
+    z = length(data(1, 1, :));
+    result = zeros(x,y,z);
+    for i = 1:z
+        all_dim = data(:,:,i);
+        all_dim = all_dim';
+        xyzPoints = data(1:3,:,i);
+        xyzPoints = xyzPoints';
+
+%%%%%%%%%%%%%%%%  reduction based on label  %%%%%%%%%%%%%%%  
+
+    %         label_path = strcat( mainpath, '/', category, '/points_label/', label_files(n).name);
+    %         lables = load(label_path);
+    %         A = (lables ~= 2);
+    
+%%%%%%%%%%%%%%%%  cut part of the object bansed on axis  %%%%%%%%%%%%%%%  
+  
+    %         [Max_v,Max_i] = max(xyzPoints);
+    %         [Min_v,Min_i] = min(xyzPoints);
+    %         Range_value = Max_v - Min_v;
+    %         [value, axis] = max(Range_value);
+    %         A = xyzPoints(:,axis) > (Min_v(axis) + value * 0.3);
+    % 
+    %         xyzPoints = xyzPoints(A ~= 0,:);
+
+%%%%%%%%%%%%%%%%  get normals  %%%%%%%%%%%%%%%    
+        ptCloud = pointCloud(xyzPoints);
+        normals = pcnormals(ptCloud);
+
+%%%%%%%%%%%%%%%%  show normals  %%%%%%%%%%%%%%%
+%         figure;
+%         pcshow(ptCloud);
+%         title('Estimated Normals of Point Cloud');
+%         hold on;
+%     
+%         x = ptCloud.Location(1:1:end,1);
+%         y = ptCloud.Location(1:1:end,2);
+%         z = ptCloud.Location(1:1:end,3);
+%         u = normals(1:1:end,1);
+%         v = normals(1:1:end,2);
+%         w = normals(1:1:end,3);
+%     
+%         quiver3(x,y,z,u,v,w);
+%         hold off
+   
+%%%%%%%%%%%%%%%%  out put normals and coords  %%%%%%%%%%%%%%%
+        coords_normal = cat(2, all_dim, normals);
+        coords_normal = coords_normal';
+        result(:,:,i) = coords_normal;
+    end
+    
+    out_path = strcat( mainpath, '/coords_normal/', data_files(n).name);
+    
+    info = h5info(data_path);
+    ChunkSize_data = info.Datasets(1).ChunkSize;
+    
+    Dataspace_label = info.Datasets(2).Dataspace.Size;
+    ChunkSize_label = info.Datasets(2).ChunkSize;
+      
+    h5create(out_path,'/data',[x y z],'Datatype','single','ChunkSize', [ChunkSize_data], 'Deflate', 4);
+    h5write(out_path,'/data',result);
+    
+    h5create(out_path,'/label',[Dataspace_label],'Datatype','uint8','ChunkSize', [ChunkSize_label],'Deflate', 1);
+    h5write(out_path,'/label',label);
+      
+    h5disp(out_path);
+end
+
