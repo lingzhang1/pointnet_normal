62d79554fac52312e57aac154e37966021e25cb3
LongLong-Jing <longlongjing.cs@gmail.com>
2018-10-12 19:05:57 -0400

    fix class normal

--- gen_class_normal_hdf5.m
+++ gen_class_normal_hdf5.m
@@ -10,18 +10,20 @@ data_files = dir(data_path);
 mkdir data/modelnet40_ply_hdf5_2048 coords_normal;
 
 for n=1:length(data_files)
-    data_path = strcat( mainpath, '/', data_files(n).name);
+    data_path = strcat( mainpath, '/',data_files(n).name);
+% %  data_files(n).name
 
-    h5disp(data_path);
+%     h5disp(data_path);
     data = h5read(data_path,'/data');
     faceId = h5read(data_path,'/faceId');
     label = h5read(data_path,'/label');
-    normal = h5read(data_path,'/normal');
+    normal = h5read(data_path,'/normal');  
 
     x = length(data(:, 1, 1)) + 3;
     y = length(data(1, :, 1));
     z = length(data(1, 1, :));
     result = zeros(x,y,z);
+    nan_num = 0;
     for i = 1:z
         xyzPoints = data(:,:,i);
         xyzPoints = xyzPoints(1:3,:);
@@ -45,21 +47,29 @@ for n=1:length(data_files)
 
 %%%%%%%%%%%%%%%%  get normals  %%%%%%%%%%%%%%%
         ptCloud = pointCloud(xyzPoints);
-        normals = pcnormals(ptCloud);
+        normals = pcnormals(ptCloud, 30);
+        
+        [row, col] = find(isnan(normals));
+        if length(row) ~= 0
+            xyzPoints(row,:) = xyzPoints(row-1,:);
+            normals(row,:) = normals(row-1,:);
+        end
+        [row, col] = find(isnan(normals));
+        nan_num = nan_num + length(row);
 
 %%%%%%%%%%%%%%%%  show normals  %%%%%%%%%%%%%%%
 %         figure;
 %         pcshow(ptCloud);
 %         title('Estimated Normals of Point Cloud');
 %         hold on;
-%
+% 
 %         x = ptCloud.Location(1:1:end,1);
 %         y = ptCloud.Location(1:1:end,2);
 %         z = ptCloud.Location(1:1:end,3);
 %         u = normals(1:1:end,1);
 %         v = normals(1:1:end,2);
 %         w = normals(1:1:end,3);
-%
+% 
 %         quiver3(x,y,z,u,v,w);
 %         hold off
 
@@ -69,7 +79,17 @@ for n=1:length(data_files)
         result(:,:,i) = coords_normal;
     end
 
-    out_path = strcat( mainpath, '/coords_normal/', data_files(n).name);
+%   
+    processing = data_files(n).name
+    nan_num
+%     Max = max(result,[],1,'includenan');
+%     Max = max(Max,[],2,'includenan');
+%     Max = max(Max,[],3,'includenan')
+%     Min = min(result,[],1,'includenan');
+%     Min = min(Min,[],2,'includenan');
+%     Min = min(Min,[],3,'includenan')
+
+    out_path = strcat( mainpath, '/coords_normal/',data_files(n).name);
 
     info = h5info(data_path);
 
@@ -97,5 +117,5 @@ for n=1:length(data_files)
     h5create(out_path,'/normal',[Dataspace_normal],'Datatype','single','ChunkSize', [ChunkSize_normal],'Deflate', 4);
     h5write(out_path,'/normal',normal);
 
-    h5disp(out_path);
+%     h5disp(out_path);
 end
