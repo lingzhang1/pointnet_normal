8c06f5a8f7ef661cc59d70f4e8a3f71caeccce0b
Ling Zhang <dearlingzhang@gmail.com>
2018-10-13 11:30:35 -0400

    fix part-seg test

--- part_seg/test.py
+++ part_seg/test.py
@@ -190,8 +190,8 @@ def predict():
 
             pts, seg = load_pts_seg_files(pts_file_to_load, seg_file_to_load, objcats[cur_gt_label])
             nan_vec = np.argwhere(np.isnan(pts))
-            print("nan_vec =", nan_vec.shape)
-            
+            print("nan_vec =", nan_vec)
+
             ori_point_num = len(seg)
 
             batch_data[0, ...] = pc_augment_to_point_num(pc_normalize(pts), point_num)
